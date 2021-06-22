6972fb969853f3685a63d2de4edbff22f9777212
PavelAvrusevich <p.avrusevich@gmail.com>
2021-06-22 16:38:01 +0300

    Main menu, user interface.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -11,16 +11,90 @@ let mainWindow;
 function createMainWindow() {
     mainWindow = new BrowserWindow({
         title: 'ImageShrink',
-        width: 500,
+        width: isDev ? 800 : 500,
         height: 600,
         icon: `${__dirname}/assets/icons/Icon_256x256.png`,
         resizable: isDev,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+        },
     });
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
 
     mainWindow.loadFile('./app/index.html');
 }
 
-app.on('ready', createMainWindow);
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        title: 'About ImageShrink',
+        width: 300,
+        height: 400,
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: false,
+    });
+
+    aboutWindow.loadFile('./app/about.html');
+}
+
+app.on('ready', () => {
+    createMainWindow();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+const menu = [
+    ...(isMac
+        ? [
+              {
+                  label: app.name,
+                  submenu: [
+                      {
+                          label: 'About',
+                          click: createAboutWindow,
+                      },
+                  ],
+              },
+          ]
+        : []),
+    {
+        role: 'fileMenu',
+    },
+    ...(!isMac
+        ? [
+              {
+                  label: 'Help',
+                  submenu: [
+                      {
+                          label: 'About',
+                          click: createAboutWindow,
+                      },
+                  ],
+              },
+          ]
+        : []),
+    ...(isDev
+        ? [
+              {
+                  label: 'Developer',
+                  submenu: [
+                      { role: 'reload' },
+                      { role: 'forcereload' },
+                      { type: 'separator' },
+                      { role: 'toggledevtools' },
+                  ],
+              },
+          ]
+        : []),
+];
 
 app.on('window-all-closed', function () {
     if (!isMac) app.quit();
