ba503020c9e2a2912435e7f7a71ac62e774a5b56
PavelAvrusevich <p.avrusevich@gmail.com>
2021-06-24 18:52:47 +0300

    Logging errors, electron-packager setup.

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const slash = require('slash');
+const log = require('electron-log');
 
 // Set env
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
@@ -123,11 +124,11 @@ async function shrinkImage({ imgPath, quality, dest }) {
             ],
         });
 
-        console.log(files);
         shell.openPath(dest);
+
         mainWindow.webContents.send('image:done');
     } catch (err) {
-        console.log(err);
+        log.error(err);
     }
 }
 
